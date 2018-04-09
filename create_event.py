b3b743e7ac8412278d80aa348b6e8a46a1b102f4
Vivek Bhave <vivbhave@gmail.com>
2018-04-09 07:09:04 +0530

    generic function calling

--- create_event.py
+++ create_event.py
@@ -2,16 +2,20 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+def but_click():
+    print("Inside generic function")
+
 class GridWindow(Gtk.Window):
     
+    def b1_click(self, widget):
+        but_click()
+
+    def b2_click(self, widget):
+        but_click()
+
     def __init__(self):
         Gtk.Window.__init__(self, title="Choose an Option")
         
-        def b1_click(self, widget):
-            print("View Calender")
-
-        def b2_click(self, widget):
-            print("Send emails")
 
         grid = Gtk.Grid()
         self.add(grid)
@@ -21,9 +25,11 @@ class GridWindow(Gtk.Window):
         button1.set_size_request(50, 100)
         button2.set_size_request(50, 100)
 
-        button1.connect("clicked", b1_click)
-        button2.connect("clicked", b2_click)        
+        button1.connect("clicked", self.b1_click)
+        button2.connect("clicked", self.b2_click)        
 
+        
+        
         grid.add(button1)
         #grid.attach(button2, 1, 0, 4, 4)
         grid.add(button2)
