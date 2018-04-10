3cac076a419aafe025d9e590c13e6057812f688c
Vivek Bhave <vivbhave@gmail.com>
2018-04-10 18:17:04 +0530

    schema updated

--- ddl.sql
+++ ddl.sql
@@ -1,8 +1,8 @@
 CREATE TABLE events (
 	event_id INT PRIMARY KEY AUTO_INCREMENT,
 	event_name VARCHAR(50),
-	start_time datetime,
-	end_time datetime
+	start_time VARCHAR(50),
+	end_time VARCHAR(50)
 );
 
 CREATE TABLE tasks (
--- event.py
+++ event.py
@@ -1,6 +1,17 @@
 import mysql.connector
+import datetime
 
-conn = mysql.connector.connect(database="events", user="root", host="127.0.0.1", password="vivbhav97")
+from dateutil import parser
 
+#conn = mysql.connector.connect(database="events", user="root", host="127.0.0.1", password="vivbhav97")
+#cursor = conn.cursor(buffered=True)
 
+start_time = raw_input("Enter start time of event\n")
+dt = parser.parse(start_time)
+
+end_time = raw_input("Enter end time of event\n")
+dt_2 = parser.parse(end_time)
+
+print (dt)
+print (dt_2)
 
