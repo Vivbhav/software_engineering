2ec97fdd8055193386d3aab9a56265f50ff48d4c
Vivek Bhave <vivbhave@gmail.com>
2018-04-12 00:22:15 +0530

    didnt add this file by mistake

--- main_code.py
+++ main_code.py
@@ -6,4 +6,16 @@ def main_code(string):
     word_list = word_tokenize(string)
     stop_words = set(stopwords.words('english'))
     
-    #if "reminder" in word_list
+    if "event" in word_list:
+        if "create" in word_list:
+		    name = word_list[2]
+
+		    start_time = 
+
+		    end_time = raw_input("Enter end time of event\n")
+		    dt_2 = parser.parse(end_time)
+
+		    cursor.execute(("insert into events_list(event_name,start_time,end_time) values('{}', '{}', '{}');".format(name, dt, dt_2)))
+		    cursor.execute(("commit;"))
+
+            
