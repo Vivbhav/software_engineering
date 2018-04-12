59de5550c5d30b1979d36c2228de49a5419c3488
amritrajck <amritrajck@gmail.com>
2018-04-12 13:51:33 +0530

    Making search Modular.

--- search.py
+++ search.py
@@ -4,14 +4,22 @@ try:
     from googlesearch import search
 except ImportError: 
     print("No module named 'google' found")
- 
-# Generates links and info as well
-query = raw_input("Enter your query- ")
-cmd = "googler "+ query
+
+def search(): 
+	# Generates links and info as well
+	query = input("Enter your query- ")
+	cmd = "googler -C "+query+" | tee out.txt"
+
+	#following Code Only Generates Links, Not Info 
+	#for j in search(query, tld="co.in", num=1, stop=1, pause=2):
+	#    print(j)
+
+	os.system(cmd)
+
+	with open('out.txt', 'r') as myfile:
+		data = myfile.read()
+		return data
 
 
-#following Code Only Generates Links, Not Info 
-#for j in search(query, tld="co.in", num=1, stop=1, pause=2):
-#    print(j)
 
-os.system(cmd)
+
