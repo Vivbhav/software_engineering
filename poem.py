a645f70eb1ee28bf62de7395ed671fc08889fd23
amritrajck <amritrajck@gmail.com>
2018-04-12 13:56:22 +0530

    Poem will now pipe to GUI. Infer variable.

--- poem.py
+++ poem.py
@@ -10,16 +10,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 import pyttsx3
 
-
-driver = webdriver.PhantomJS() #For Visible window use Firefox
-driver.get('https://www.poets.org/poetsorg/poem-day')
-time.sleep(2)
-header = driver.find_elements_by_xpath("//*[@id='page-title']")[1].text
-content = driver.find_element_by_tag_name('pre').text
-
-print(header)
-
-print(content)
-to_return = (header, content)
-driver.close()
-return to_return
+def poem():
+	driver = webdriver.PhantomJS() #For Visible window use Firefox
+	driver.get('https://www.poets.org/poetsorg/poem-day')
+	time.sleep(2)
+	header = driver.find_elements_by_xpath("//*[@id='page-title']")[1].text
+	content = driver.find_element_by_tag_name('pre').text
+	out = header+"\n"+content
+	driver.close()
+	return(out)
