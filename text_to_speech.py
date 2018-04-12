b4c7a9ad8f697e4cb7bd1227446f7ba18d82076d
Vivek Bhave <vivbhave@gmail.com>
2018-04-12 22:22:18 +0530

    tts verified

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,9 +2,9 @@ import speech_recognition as sr
 from time import ctime
 import time
 import os
-from gtts import gTTS
-import pyttsx3
-
+#from gtts import gTTS
+#import pyttsx3
+import pyttsx
 
 def speak(audioString):
     print(audioString)
@@ -17,4 +17,9 @@ def speak1(audioString):
     engine.say(audioString)
     engine.runAndWait
 
+def speak2(audioString):
+    engine = pyttsx.init()
+    engine.say(audioString)
+    engine.runAndWait()
 
+speak2("my name is vivek")
